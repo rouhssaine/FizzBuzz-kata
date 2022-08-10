a137e911a516163ea7c30488580e21bb558a4388
Rachid OUHSSAINE <rachid.ouhssaine@outlook.com>
2022-08-10 17:11:06 +0200

    [FizzBuzz] Second step : 2 REFACTOR

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,31 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayNameGeneration(ReplaceUnderscores.class)
 class FizzBuzzTest {
+
+    FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setup() {
+        fizzBuzz = new FizzBuzz();
+    }
 
     @Test
     void should_return_one_when_input_one() {
-        //Given
-        int input = 1;
-
-        //When
-        var result = new FizzBuzz().of(input);
-
-        //Then
-        assertThat(result).isEqualTo(1);
+        assertThat(fizzBuzz.of(1)).isEqualTo(1);
     }
 
     @Test
     void should_return_two_when_input_two() {
-        //Given
-        int input = 2;
-
-        //When
-        var result = new FizzBuzz().of(input);
-
-        //Then
-        assertThat(result).isEqualTo(2);
+        assertThat(fizzBuzz.of(2)).isEqualTo(2);
     }
 
 }
