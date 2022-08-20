1e733633508a0583f8c7727693fff53694fa2a12
Rachid OUHSSAINE <rachid.ouhssaine@outlook.com>
2022-08-20 13:07:04 +0200

    [FizzBuzz] REFACTORING

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,3 +1,7 @@
+import com.google.common.base.Strings;
+
+import java.util.Optional;
+
 import static java.lang.String.valueOf;
 
 public class FizzBuzz {
@@ -8,14 +12,21 @@ public class FizzBuzz {
     public static final int BUZZ_VALUE = 5;
 
     public String of(int value) {
-        if (isMultipleOf(value, FIZZ_VALUE) && isMultipleOf(value, BUZZ_VALUE)) return FIZZ+BUZZ;
-        if (isMultipleOf(value, FIZZ_VALUE)) return FIZZ;
-        if (isMultipleOf(value, BUZZ_VALUE)) return BUZZ;
-        return valueOf(value);
+        StringBuilder fizzBuzzBuilder = new StringBuilder();
+        if (isMultipleOf(value, FIZZ_VALUE)) fizzBuzzBuilder.append(FIZZ);
+        if (isMultipleOf(value, BUZZ_VALUE)) fizzBuzzBuilder.append(BUZZ);
+        return fizzBuzzOf(value, fizzBuzzBuilder);
     }
 
     private boolean isMultipleOf(int number, int divider) {
         return number % divider == 0;
+    }
+
+    private String fizzBuzzOf(int value, StringBuilder fizzBuzzBuilder) {
+        return Optional.of(fizzBuzzBuilder)
+                .map(StringBuilder::toString)
+                .map(Strings::emptyToNull)
+                .orElse(valueOf(value));
     }
 
 }
