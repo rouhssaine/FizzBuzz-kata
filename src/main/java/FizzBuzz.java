0998af2bcf344065703e1b34b7f0b28acbf0afc4
Rachid OUHSSAINE <rachid-externe.ouhssaine@datanumia.com>
2023-05-07 18:36:26 +0200

    [FizzBuzz] REFACTORING

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,20 +1,38 @@
+import com.google.common.base.Strings;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Set;
+
 import static java.lang.String.valueOf;
+import static java.util.Map.Entry.comparingByKey;
+import static java.util.stream.Collectors.joining;
 
 public class FizzBuzz {
 
-    public static final String FIZZ = "Fizz";
-    public static final String BUZZ = "Buzz";
-    public static final int FIZZ_VALUE = 3;
-    public static final int BUZZ_VALUE = 5;
+    private static final Map<Integer, String> VALUE_BY_DIVIDER = Map.of
+            (3, "Fizz",
+             5, "Buzz");
 
-    public String of(int value) {
-        StringBuilder fizzBuzzBuilder = new StringBuilder();
-        if (isMultipleOf(value, FIZZ_VALUE)) fizzBuzzBuilder.append(FIZZ);
-        if (isMultipleOf(value, BUZZ_VALUE)) fizzBuzzBuilder.append(BUZZ);
-        return fizzBuzzBuilder.isEmpty() ? valueOf(value) : fizzBuzzBuilder.toString();
+    public String of(int number) {
+        return convert(number)
+                .orElse(valueOf(number));
     }
 
-    private boolean isMultipleOf(int number, int divider) {
+    private static Optional<String> convert(int value) {
+        return Optional.of(VALUE_BY_DIVIDER)
+                .map(Map::entrySet)
+                .map(Set::stream)
+                .map(stream -> stream
+                        .sorted(comparingByKey())
+                        .filter(entry -> isMultipleOf(value, entry.getKey()))
+                        .map(Entry::getValue)
+                        .collect(joining()))
+                .map(Strings::emptyToNull);
+    }
+
+    private static boolean isMultipleOf(int number, int divider) {
         return number % divider == 0;
     }
 
